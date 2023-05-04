bad1d9236244f37086fad80ec20e939cc544814a
bryangwin <bryangwin@gmail.com>
2023-05-04 15:50:57 -0700

    Initial Commit

---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Bryan Gwin on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
